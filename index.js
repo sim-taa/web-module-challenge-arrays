73561d19d4ae87e354d9f9e6b3f021036a0bf873
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-01-12 13:26:10 -0800

    solves task 2

--- index.js
+++ index.js
@@ -64,8 +64,12 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 // use a 1 parameter array
-function is31Flavors(/*your code here*/){
- /*your code here*/
+function is31Flavors(array){
+ if (array.length === 31) {
+   return true;
+ }else{
+   return false;
+ }
  // we want to check to see that the length of the array is equal to 31. if it is, return true. else, return false.
 }
 
