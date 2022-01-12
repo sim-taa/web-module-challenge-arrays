9a4d38511ca26f566d6bbd825e2094a854cde781
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-01-12 13:45:30 -0800

    solves tasks 3 and 4

--- index.js
+++ index.js
@@ -85,12 +85,12 @@ Use the addFlavor function below to do the following:
   For example: addFlavor(originalFlavors, "Rainbow Sherbert") should return the array ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"]
 */
 
-// 2 parameter array, second is new flavor 
+// 2 parameter: first is array, second is new flavor 
 //use unshift to add to the front of an array and return the array
-function addFlavor(/*your code here*/){
- /*your code here*/
+function addFlavor(array,flavor){
+  array.unshift(flavor);
+  return array;
 }
-
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Houston, we have a problem! There are now 32 flavors in the originalFlavors array! Your task is to remove an item from the end of the array. 
@@ -103,8 +103,9 @@ Use the removeLastFlavor function below to do the following:
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
 
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+function removeLastFlavor(array){
+ array.pop()
+ return array
 }
 
 
